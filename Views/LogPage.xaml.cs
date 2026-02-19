b54ac8c7b09ff54cc02832b34147e09e03a493e3
sagaciousness2022 <a352686002@gmail.com>
2026-02-20 05:16:28 +0800

    重构 UI 架构并新增实验显示控制

---
+++ Views/LogPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace QuestADBTool.Views;
+
+public partial class LogPage : UserControl
+{
+    public LogPage()
+    {
+        InitializeComponent();
+    }
+
+    public Button ToggleLogButtonControl => ToggleLogButton;
+    public StackPanel LogBodyPanelControl => LogBodyPanel;
+    public Button AuthorButtonControl => AuthorButton;
+    public Button ClearLogButtonControl => ClearLogButton;
+    public Button CopyLogButtonControl => CopyLogButton;
+    public TextBox LogBoxControl => LogBox;
+}
